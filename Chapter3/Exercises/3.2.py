c42b0a5c9268b247ce172a040c65dd7a48edafcb
cbhutad <learningcbhutad@gmail.com>
2023-06-12 22:54:08 +0530

    Added Chapter3 folder

---
+++ Chapter3/Exercises/3.2.py
@@ -0,0 +1,10 @@
+names = ["Rajas", "Kalpak", "Prathamesh"]
+
+message = f"Hello, {names[0]}"
+print(message)
+
+message = f"Hello, {names[1]}"
+print(message)
+
+message = f"Hello, {names[2]}"
+print(message)
