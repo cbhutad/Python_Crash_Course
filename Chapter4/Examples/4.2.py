7113246ae1b198059c8104cf7167edb055433146
cbhutad <learningcbhutad@gmail.com>
2023-06-19 22:44:16 +0530

    new Examples on list manipulation

---
+++ Chapter4/Examples/4.2.py
@@ -0,0 +1,25 @@
+numbers = list(range(1,10))
+
+for number in numbers:
+	print(number)
+
+
+
+
+
+
+
+
+
+
+
+print("Hello in the middle")
+
+
+
+
+
+
+
+
+	print("Well this works fine")
---
+++ Chapter4/Examples/4.3.py
@@ -0,0 +1,3 @@
+numbers = list(range(1,11))
+
+print(numbers[-3:-6])
