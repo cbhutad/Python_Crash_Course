a8f5d7f789e783c36105445ba365f93b7c73ec55
cbhutad <learningcbhutad@gmail.com>
2023-06-27 22:30:08 +0530

    Started Chapter 7

---
+++ Chapter7/Exercises/7.1.py
@@ -0,0 +1,2 @@
+car = input("Hello sir, what kind of car are you looking for : ")
+print(f"We have here the latest model for the {car} you are	looking for")
---
+++ Chapter7/Exercises/7.3.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number : "))
+
+if number % 10 == 0:
+	print("The number is divisible by 10")
+else:
+	print("The number is not divisible by 10")
