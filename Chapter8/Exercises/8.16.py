e7ae8fc62d6b704f0f45247b502db25cb32e8861
cbhutad <learningcbhutad@gmail.com>
2023-07-08 15:11:24 +0530

    Completed 8.16 exercise

---
+++ Chapter8/Exercises/8.16.py
@@ -0,0 +1,11 @@
+#import random
+#from random import randint
+#from random import randint as randomInteger
+#import random as rnd
+from random import *
+
+#print(random.randint(0,10))
+#print(randint(0, 10))
+#print(randomInteger(0, 10))
+#print(rnd.randint(0,10))
+print(randint(0,4))
