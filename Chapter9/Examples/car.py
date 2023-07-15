f4f56f0ad3eb1e177dec07d423af0e25a07545e3
cbhutad <learningcbhutad@gmail.com>
2023-07-15 20:24:27 +0530

    Ways to update attributes examples

---
+++ Chapter9/Examples/car.py
@@ -0,0 +1,52 @@
+class Car:
+
+    def __init__(self,make,model,year):
+        """Initialize attributes for instances"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a descriptive name"""
+        long_name = f"{self.model} {self.make} {self.year}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """prints the odometer reading"""
+        print(f"The car has {self.odometer_reading} miles on it")
+
+    def update_odometer(self,mileage):
+        if mileage > self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You cannot roll the odometer back")
+
+    def increment_odometer(self,miles):
+        if miles > 0:
+            self.odometer_reading += miles
+        else:
+            print("Miles entered are invalid")
+
+used_car = Car("subaru", "outback", 2015)
+print(used_car.get_descriptive_name())
+print()
+
+used_car.read_odometer()
+print()
+
+used_car.update_odometer(23)
+used_car.read_odometer()
+print()
+
+used_car.update_odometer(20)
+print()
+
+used_car.increment_odometer(30)
+used_car.read_odometer()
+print()
+
+used_car.increment_odometer(-10)
+print()
+
+
