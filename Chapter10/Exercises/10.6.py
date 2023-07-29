dd0a07dc35254d4b1945a0d32aa8b9f007a9f1da
cbhutad <learningcbhutad@gmail.com>
2023-07-29 11:06:28 +0530

    Completed 10.6, 10.7, 10.8, 10.9, 10.10 exercises

---
+++ Chapter10/Exercises/10.6.py
@@ -0,0 +1,16 @@
+print("enter 2 numbers to add")
+print("enter q to quit")
+
+
+num1 = input("Enter first number : ")
+if num1 == "q":
+    break
+num2 = input("Enter second number : ")
+if num2 == "q":
+    break
+try:
+    result = int(num1) + int(num2)
+except ValueError:
+    print("Number cannot be in text format")
+else:
+    print(f"Sum is {result}")
---
+++ Chapter10/Exercises/10.8.py
@@ -0,0 +1,16 @@
+file1 = "input_files\cat.txt"
+file2 = "input_files\dogs.txt"
+
+def file_processing(filename):
+    try:
+        with open(file,"r") as file_obj:
+            contents = file_obj.readlines()
+    except FileNotFoundError:
+        print(f"given {filename} does not exist at the path")
+    else:
+        for line in contents:
+            print(line.strip())
+        print()
+
+for file in [file1,file2]:
+    file_processing(file)
