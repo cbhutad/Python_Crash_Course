50e05fe21e1dfd3113ec14479b4948685cc6299d
cbhutad <learningcbhutad@gmail.com>
2023-07-31 23:00:10 +0530

    files deleted

--- Chapter11/Exercises/city_functions.py
+++
@@ -1,6 +0,0 @@
-def city_country(city, country, population=""):
-    """Returns the city, country format string in a capitalized format"""
-
-    if population != "":
-        return f"{city.title()}, {country.title()} - population {population}"
-    return f"{city}, {country}".title()
